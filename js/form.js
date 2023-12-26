37beb01c294e5aa12ba7588bf7acb5e22315d2e6
adityabhutada08 <080202adi.bhutada@gmail.com>
2023-12-26 11:03:36 +0530

    Created the Form which sends email in contact us section and do changes in index file according to form

---
+++ js/form.js
@@ -0,0 +1,30 @@
+// Contact Form
+$(document).ready(function() {
+    $('#contactForm').submit(function(event) {
+      event.preventDefault(); // Prevent the default form submission
+      
+      // Get form data
+      var formData = $(this).serialize();
+  
+      // Make AJAX call to submit the form data
+      $.ajax({
+        type: 'POST', // Or 'GET', depending on your server setup
+        url: 'http://localhost:5000/send_email', // Replace with your server endpoint
+        data: formData,
+        success: function(response) {
+          // On success, show the success message and mark
+          $('#submitStatus').show();
+        },
+        error: function(err) {
+          // On error, show an error message or handle it accordingly
+          console.error('Error submitting form:', err);
+        }
+      });
+    });
+  });
+  
+
+
+
+
+
